73aacce3f23a74e641ad6c2211daeb5298afda5d
laurelr2020 <reganlaurell@gmail.com>
2019-11-21 21:55:25 -0500

    working on base case

--- src/sierpinskitriangles/SierpinskiTriangles.java
+++ src/sierpinskitriangles/SierpinskiTriangles.java
@@ -3,7 +3,7 @@ package sierpinskitriangles;
 import java.awt.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.Scanner;
+import java.awt.geom.Line2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,22 +12,37 @@ import javax.swing.JPanel;
  */
 
 public class SierpinskiTriangles extends JPanel {
-    public static final int SIZE = 600;  // height/width of DrawingPanel
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 600;
+    private static final int BASE_LEVEL = 1;
 
     public SierpinskiTriangles(){
-        setPreferredSize(new Dimension(SIZE, SIZE));
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setName("CSC 380 Graphics Problem 2 -- Regan and Joe");
         setUp();
-        
     }
     
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g); 
         Graphics2D g2 = (Graphics2D) g.create();
+        
+        Line2D line1 = new Line2D.Double(0.0, 200.0, 100.0, 200.0);
+        Line2D line2 = new Line2D.Double(100.0, 300.0, 200.0, 300.0);
+        Line2D line3 = new Line2D.Double(200.0, 400.0, 300.0, 400.0);
 
-
+        drawTriangleLevel(g2, BASE_LEVEL, line1, line2, line3);
     }
+    
+    private void drawTriangleLevel(Graphics2D g2, int level, Line2D line1, Line2D line2, Line2D line3){
+        if(level == 1){
+            //base case
+            Line2D line = new Line2D.Double();
+           
+        }
+    }
+    
+    
     
     /***********************************************
      * Do NOT change or delete anything below here!
